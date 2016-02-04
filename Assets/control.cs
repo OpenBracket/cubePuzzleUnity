ff6588ec0c2e15c578d13603d659e5f8da4f7982
Jellyman305 <robpetrie4@icloud.com>
2016-02-04 20:01:12 +0000

    first commit

---
+++ Assets/control.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class control : MonoBehaviour {
+
+	Rigidbody2D cube;
+	public float speed, jump;
+
+	// Use this for initialization
+	void Start () {
+		cube = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.D)) {
+			//cube.MovePosition (cube.position + new Vector2(speed,0));
+			cube.AddForce (new Vector2(speed,0), ForceMode2D.Impulse);
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			//cube.MovePosition (cube.position + new Vector2(-speed,0));
+			cube.AddForce (new Vector2(-speed,0), ForceMode2D.Impulse);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			cube.AddForce (new Vector2(0,jump), ForceMode2D.Impulse);
+		}
+	
+	}
+}
---
+++ Assets/gen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gen : MonoBehaviour {
+
+	public GameObject block;
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		offset = new Vector3 (0,0,0);
+
+		for (int i = 0; i < 100; i++) {
+
+			Instantiate (block, transform.position + offset, Quaternion.identity);
+			offset += new Vector3 (1.25f,0,0);
+
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
