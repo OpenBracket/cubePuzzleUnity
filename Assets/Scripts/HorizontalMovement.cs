c32c5a0af9450025c53b3b752193a5c011935b14
11wykessam <samuelwykes@live.co.uk>
2016-02-20 00:11:00 +0000

    Movement Change
    Horizontal and Vertical scripts separated, force is now used for jump,
    this needs to be made less jittery.

---
+++ Assets/Scripts/HorizontalMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;
+using System.Collections;
+
+public class HorizontalMovement : MonoBehaviour {
+
+	public float Speed = 0f;
+	private float movex = 0f;
+	private float movey = 0f;
+	private float yVel = 0f;
+
+	Rigidbody2D player;
+
+	// Use this for initialization
+	void Start()
+	{
+		player = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+
+		movex = Input.GetAxis("Horizontal");
+		//movey = Input.GetAxis("Vertical");
+
+		player.velocity = new Vector2(movex * Speed, yVel);
+
+	}
+}
---
+++ Assets/Scripts/VerticalMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticalMovement : MonoBehaviour {
+	public float Speed = 0f;
+	public float jump = 0f;
+	private float movex = 0f;
+	private float movey = 0f;
+	private float yVel = 0f;
+
+	Rigidbody2D player;
+
+	// Use this for initialization
+	void Start()
+	{
+		player = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			player.AddForce (new Vector2(0,jump), ForceMode2D.Impulse);
+
+		}
+
+
+
+	}
+}
