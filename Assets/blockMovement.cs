6581c19dd64bf3766f94e383c26ff32ceb0e2202
Jellyman305 <robpetrie4@icloud.com>
2016-03-01 20:30:42 +0000

    basics for connecting blocks

---
+++ Assets/blockMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blockMovement : MonoBehaviour {
+
+    public static GameObject playerRight, playerLeft, playerTop, playerBottom;
+    public bool isConnected;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if(isConnected)
+        {
+            enableMovement();
+        }
+
+    }
+
+    void enableMovement()
+    {
+        this.GetComponent<HorizontalMovement>().enabled = true;
+        this.GetComponent<VerticalMovement>().enabled = true;
+    }
+}
